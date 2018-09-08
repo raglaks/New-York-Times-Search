fcb7a4bc5df8d75696132fc3f48133851741b73b
Edgar Burelo <edgar.burelo@gmail.com>
2018-09-08 13:06:11 -0500

    js

--- backend.js
+++ backend.js
@@ -1,15 +1,13 @@
-
-
-
-
 $.ajax({
     url: "https://api.nytimes.com/svc/search/v2/articlesearch.json",
     method: "GET",
     data: {"api-key":"688ebf136fc34c92aa7ab402ce7123df", 'q': "obama"}
   }).then(function(rensonse) {
     console.log(rensonse);
-  });
+});
 
-  $("").on ("click",function(event) {
-    console.log(event);
+  $(".btn").on("click",function() {
+    event.preventDefault();
+    var algo = $("#records option:selected").text();
+    console.log(algo);
   });
